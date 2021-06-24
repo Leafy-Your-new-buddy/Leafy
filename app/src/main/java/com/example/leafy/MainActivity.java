e7a285d27af0ca02d2eec9840516a2d2c6a03a98
jiwon199 <jiwonchoi99@naver.com>
2021-06-24 12:56:13 +0900

    first commit

---
+++ app/src/main/java/com/example/leafy/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.leafy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView navigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnNavigationItemSelectedListener(listener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new MainFragment()).commit();
+        navigationView.getMenu().getItem(1).setChecked(true);
+
+    }
+
+    //하단 메뉴바.
+    private BottomNavigationView.OnNavigationItemSelectedListener listener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selected_fragment=null;
+            switch (item.getItemId()){
+                case R.id.calendar:
+                    selected_fragment=new CalendarFragment();
+                    break;
+                case R.id.home:
+                    selected_fragment=new MainFragment();
+                    break;
+                case R.id.camera:
+                    selected_fragment=new CameraFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,selected_fragment).commit();
+
+            return true;
+        }
+    };
+
+
+
+
+}
