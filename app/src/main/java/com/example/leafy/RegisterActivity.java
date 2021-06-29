c5a47731092ea8bddcc8b97a6dff8efeb58cedd0
jiwon199 <jiwonchoi99@naver.com>
2021-06-29 16:30:20 +0900

    카메라임시버튼 튕김 해결

--- app/src/main/java/com/example/leafy/RegisterActivity.java
+++ app/src/main/java/com/example/leafy/RegisterActivity.java
@@ -3,9 +3,18 @@ package com.example.leafy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
+    private DatabaseReference mDatabaseRef;  //실시간 데이터베이스
+    private EditText mEtEmail,mEtPwd;  //회원가입 입력필드
+    private Button mBtnRegister;  //회원가입 버튼
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
