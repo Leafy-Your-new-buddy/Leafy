02baa70ce9197d758fc08dcd2135ec98d3739bc1
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-07-08 14:13:52 +0900

    상태진단 CameraFragment에서 동작하도록 수정 + 카메라 임시버튼 삭제 + 날씨정보 받아오기 toast 메시지 수정

--- app/src/main/java/com/example/leafy/CameraFragment.java
+++ app/src/main/java/com/example/leafy/CameraFragment.java
@@ -1,24 +1,93 @@
 package com.example.leafy;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorOperator;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import static androidx.core.content.ContextCompat.checkSelfPermission;
 
 
 public class CameraFragment extends Fragment implements View.OnClickListener {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 672;
+    private String imageFilePath;
+    private Uri photoUri;
+
+    protected Interpreter tflite;
+    private MappedByteBuffer tfliteModel;
+    private TensorImage inputImageBuffer;
+    private  int imageSizeX;
+    private  int imageSizeY;
+    private TensorBuffer outputProbabilityBuffer;
+    private TensorProcessor probabilityProcessor;
+    private static final float IMAGE_MEAN = 0.0f;
+    private static final float IMAGE_STD = 1.0f;
+    private static final float PROBABILITY_MEAN = 0.0f;
+    private static final float PROBABILITY_STD = 255.0f;
+    private Bitmap bitmap;
+    private List<String> labels;
+    private Context context;
+
+    Uri imageuri;
+    TextView tv_result;
+    Button btn_classify;
+    ImageView iv_result;
+
+
+    // tedpermission 대신 추가
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
 
     public CameraFragment() {
         // Required empty public constructor
     }
 
-    Button btn_camera;
+//    Button btn_camera;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,20 +96,198 @@ public class CameraFragment extends Fragment implements View.OnClickListener {
 
         View view=null;//Fragment가 보여줄 View 객체를 참조할 참조변수
         view= inflater.inflate(R.layout.fragment_camera, null);
-        btn_camera=view.findViewById(R.id.btn_camera);
-        btn_camera.setOnClickListener(this);
 
+        tv_result = view.findViewById(R.id.tv_result);
+        btn_classify = view.findViewById(R.id.classify);
+
+        context = container.getContext();
+
+        // tedpermission 대신 추가
+        if (checkSelfPermission(getContext(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+
+
+        try{
+            tflite = new Interpreter(loadmodelfile(this));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        view.findViewById(R.id.btn_capture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getActivity().getPackageManager())!=null){
+                    File photoFile = null;
+                    try{
+                        photoFile = createImageFile();
+                    } catch(IOException e){
+
+                    }
+
+                    if(photoFile != null){
+                        photoUri = FileProvider.getUriForFile(getActivity().getApplicationContext(), getActivity().getPackageName(),photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+                        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
+            }
+        });
+
+        btn_classify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int imageTensorIndex = 0;
+                int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
+                imageSizeY = imageShape[1];
+                imageSizeX = imageShape[2];
+                DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+
+                int probabilityTensorIndex = 0;
+                int[] probabilityShape =
+                        tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
+                DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
+
+                inputImageBuffer = new TensorImage(imageDataType);
+                outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+                probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
+
+                inputImageBuffer = loadImage(bitmap);
+
+                tflite.run(inputImageBuffer.getBuffer(),outputProbabilityBuffer.getBuffer().rewind());
+                showresult();
+            }
+        });
         return view;
     }
 
+    private TensorImage loadImage(final Bitmap bitmap) {
+        // Loads bitmap into a TensorImage.
+        inputImageBuffer.load(bitmap);
+
+        // Creates processor for the TensorImage.
+        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        // TODO(b/143564309): Fuse ops inside ImageProcessor.
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                        .add(getPreprocessNormalizeOp())
+                        .build();
+        return imageProcessor.process(inputImageBuffer);
+    }
+
+    private MappedByteBuffer loadmodelfile(CameraFragment cameraActivity) throws IOException{
+        AssetFileDescriptor fileDescriptor=cameraActivity.getActivity().getAssets().openFd("model.tflite");
+        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel=inputStream.getChannel();
+        long startoffset = fileDescriptor.getStartOffset();
+        long declaredLength=fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startoffset,declaredLength);
+    }
+
+    private TensorOperator getPreprocessNormalizeOp() {
+        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+    }
+    private TensorOperator getPostprocessNormalizeOp(){
+        return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
+    }
+
+    private void showresult(){
+
+        try{
+            labels = FileUtil.loadLabels(getActivity(),"label.txt");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Map<String, Float> labeledProbability =
+                new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
+                        .getMapWithFloatValue();
+        float maxValueInMap =(Collections.max(labeledProbability.values()));
+
+        for (Map.Entry<String, Float> entry : labeledProbability.entrySet()) {
+            if (entry.getValue()==maxValueInMap) {
+                tv_result.setText(entry.getKey());
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_"+timeStamp+"_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,".jpg",storageDir
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_camera:
-                Intent intent = new Intent(getActivity(),CameraActivity.class);
-                startActivity(intent);
-                break;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+            bitmap = BitmapFactory.decodeFile(imageFilePath);
+            ExifInterface exif = null;
+            try {
+                exif = new ExifInterface(imageFilePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int exifOrientation;
+            int exifDegree;
+
+            if (exif != null) {
+                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                exifDegree = exifOrientationToDegrees(exifOrientation);
+            } else {
+                exifDegree = 0;
+            }
+            iv_result = getView().findViewById(R.id.iv_result);
+            iv_result.setImageBitmap(rotate(bitmap, exifDegree));
         }
+    }
+
+    private int exifOrientationToDegrees(int exifOrientation){
+        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180){
+            return 180;
+        } else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270){
+            return 270;
+        }
+        return 0;
+    }
+
+    private Bitmap rotate(Bitmap bitmap, float degree){
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(), bitmap.getHeight(), matrix,true);
+    }
+
+    // tedpermission 대신 추가
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(context, "camera permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    @Override
+    public void onClick(View v) {
+
+//        switch (v.getId()) {
+//            case R.id.btn_camera:
+//                Intent intent = new Intent(getActivity(),CameraActivity.class);
+//                startActivity(intent);
+//                break;
+//
+//        }
     }
 }
